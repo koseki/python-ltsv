5bdc75ede757a535b947bf1d307cf2e9b9a8e896
KOSEKI Kengo <koseki@gmail.com>
2013-10-03 18:56:20 +0900

    Rename test method names and use dump method instead of writer

--- ltsv/test/test_writer.py
+++ ltsv/test/test_writer.py
@@ -1,10 +1,14 @@
 # -*- encoding: utf-8 -*-
 
-from ltsv import writer
+from ltsv import writer, dump
 from collections import OrderedDict
 
 
-def test_writer_empty():
+def test_dump_empty_dict():
+    assert '' == dump({})
+
+
+def test_writer_empty_dict():
     assert '' == writer({})
 
 
@@ -12,16 +16,16 @@ def test_writer_empty_list():
     assert '' == writer([])
 
 
-def test_writer():
-    result = writer({'aaa': 'bbb', 'ccc': 'ddd', 'eee': '123'})
+def test_dump():
+    result = dump({'aaa': 'bbb', 'ccc': 'ddd', 'eee': '123'})
     result = sorted(result.split("\t"))
     expect = ['aaa:bbb', 'ccc:ddd', 'eee:123']
     assert expect == result
 
 
-def test_writer_ordered_dict():
+def test_ordered_dict():
     odict = OrderedDict([('bcd', 'efg'), ('abc', 'def'), ('3', '444')])
-    result = writer(odict)
+    result = dump(odict)
     expect = "bcd:efg\tabc:def\t3:444"
     assert expect == result
 
@@ -33,30 +37,30 @@ def test_writer_list():
     assert expect == result
 
 
-def test_writer_int():
-    result = writer({'bbb': 111, 'aaa': 222, 3: 444})
+def test_int_key_and_value():
+    result = dump({'bbb': 111, 'aaa': 222, 3: 444})
     result = sorted(result.split("\t"))
     expect = ['3:444', 'aaa:222', 'bbb:111']
     assert expect == result
 
 
-def test_writer_none():
-    result = writer({'a': None, 'b': None})
+def test_none_value():
+    result = dump({'a': None, 'b': None})
     assert "a:\tb:" == result or "b:\ta:" == result
 
 
-def test_writer_unicode():
-    result = writer({u'あ': u'い', u'う': u'え'})
+def test_unicode_key_and_value():
+    result = dump({u'あ': u'い', u'う': u'え'})
     assert isinstance(result, unicode)
     assert u"あ:い\tう:え" == result or u"う:え\tあ:い" == result
 
 
-def test_writer_8bit_to_unicode():
-    result = writer({'a': 'b', 'c': 'd'})
+def test_string_to_unicode():
+    result = dump({'a': 'b', 'c': 'd'})
     assert isinstance(result, unicode)
 
 
-def test_writer_encoding():
+def test_encoding():
     result = writer({u'あ': u'い'}, encoding='utf-8')
     assert isinstance(result, str)
     assert "\xe3\x81\x82:\xe3\x81\x84" == result
