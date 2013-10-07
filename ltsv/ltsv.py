442f9ad66db2586a468c3a67cb5a0dd8a51ce012
KOSEKI Kengo <koseki@gmail.com>
2013-10-08 03:08:24 +0900

    Run tests with python 2.6 and 3, and fix some bugs

--- ltsv/ltsv.py
+++ ltsv/ltsv.py
@@ -9,15 +9,19 @@ def writer(obj, lineterminator="\n", encoding="utf-8"):
     elif isinstance(obj, list):
         out = []
         for line in obj:
-            out.append(dump(line, encoding))
-        return lineterminator.join(out)
+            out.append(dump(line))
+        lines = lineterminator.join(out)
+        if encoding is None:
+            return lines
+        else:
+            return lines.encode(encoding)
     else:
         raise ValueError("not support")
 
 
 def dump(dic, encoding=None):
     values = []
-    unicode_type = unicode if sys.version < '3' else str
+    unicode_type = unicode if sys.version_info < (3, 0) else str
     for k, v in dic.items():
         k = k if isinstance(k, unicode_type) else str(k)
         if v is None:
@@ -25,18 +29,18 @@ def dump(dic, encoding=None):
         elif not isinstance(v, unicode_type):
             v = str(v)
 
-            # Replace special character to space.
-            # This is not LTSV normative but 'fail-safe' treatment.
-            # User can escape values by own way before passing this method,
-            # if required. For example:
-            #
-            #   value = value.replace("\\", "\\\\")
-            #   value = value.replace("\t", "\\t")
-            #   value = value.replace("\r", "\\r")
-            #   value = value.replace("\n", "\\n")
-            #
-            v = v.replace("\t", " ")
-            v = v.replace("\r\n", " ").replace("\r", " ").replace("\n", " ")
+        # Replace special character to space.
+        # This is not LTSV normative but 'fail-safe' treatment.
+        # User can escape values by own way before passing this method,
+        # if required. For example:
+        #
+        #   value = value.replace("\\", "\\\\")
+        #   value = value.replace("\t", "\\t")
+        #   value = value.replace("\r", "\\r")
+        #   value = value.replace("\n", "\\n")
+        #
+        v = v.replace("\t", " ")
+        v = v.replace("\r\n", " ").replace("\r", " ").replace("\n", " ")
 
         values.append(k + ":" + v)
 
--- ltsv/test/test_writer.py
+++ ltsv/test/test_writer.py
@@ -1,7 +1,20 @@
 # -*- encoding: utf-8 -*-
 
+import sys
+import pytest
 from ltsv import writer, dump
-from collections import OrderedDict
+
+try:
+    from collections import OrderedDict
+except ImportError:
+    pass
+
+if sys.version_info < (3, 0):
+    UNICODE_TYPE = unicode
+    BYTES_TYPE = str
+else:
+    UNICODE_TYPE = str
+    BYTES_TYPE = bytes
 
 
 def test_dump_empty_dict():
@@ -9,11 +22,11 @@ def test_dump_empty_dict():
 
 
 def test_writer_empty_dict():
-    assert '' == writer({})
+    assert b'' == writer({})
 
 
 def test_writer_empty_list():
-    assert '' == writer([])
+    assert b'' == writer([])
 
 
 def test_dump():
@@ -23,6 +36,7 @@ def test_dump():
     assert expect == result
 
 
+@pytest.mark.skipif("sys.version_info < (2, 7)")
 def test_ordered_dict():
     odict = OrderedDict([('bcd', 'efg'), ('abc', 'def'), ('3', '444')])
     result = dump(odict)
@@ -31,10 +45,10 @@ def test_ordered_dict():
 
 
 def test_writer_list():
-    odict = OrderedDict([('bcd', 'efg'), ('abc', 'def')])
-    result = writer([odict, {'eee': '123'}])
-    expect = "bcd:efg\tabc:def\neee:123"
-    assert expect == result
+    result = writer([{'bcd': 'efg', 'abc': 'def'}, {'eee': '123'}])
+    expect1 = b"abc:def\tbcd:efg\neee:123"
+    expect2 = b"bcd:efg\tabc:def\neee:123"
+    assert expect1 == result or expect2 == result
 
 
 def test_int_key_and_value():
@@ -51,7 +65,7 @@ def test_none_value():
 
 def test_unicode_key_and_value():
     result = dump({u'あ': u'い', u'う': u'え'})
-    assert isinstance(result, unicode)
+    assert isinstance(result, UNICODE_TYPE)
     assert u"あ:い\tう:え" == result or u"う:え\tあ:い" == result
 
 
@@ -62,19 +76,19 @@ def test_not_unicode():
 
 def test_join_str_and_unicode():
     result = dump({'a': 'b', u'あ': 123})
-    assert isinstance(result, unicode)
+    assert isinstance(result, UNICODE_TYPE)
 
 
 def test_encoding_unicode():
     result = writer({u'あ': u'い'}, encoding='utf-8')
-    assert isinstance(result, str)
-    assert "\xe3\x81\x82:\xe3\x81\x84" == result
+    assert isinstance(result, BYTES_TYPE)
+    assert b"\xe3\x81\x82:\xe3\x81\x84" == result
 
 
 def test_encoding_str():
     result = writer({'a': 'b'}, encoding='utf-8')
-    assert isinstance(result, str)
-    assert "a:b" == result
+    assert isinstance(result, BYTES_TYPE)
+    assert b"a:b" == result
 
 
 def test_replace_special_characters():
